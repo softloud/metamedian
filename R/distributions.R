66a124c2779f8aa1448f97d5b4610633a6f78d3a
Charles T. Gray <C.Gray@latrobe.edu.au>
2017-11-07 20:43:01 +1100

    :cat2: Trials parameters dataframe created. :notes:

---
+++ R/distributions.R
@@ -0,0 +1,11 @@
+#' Distributions the Data was Generated From.
+#'
+#' A tibble comprising the density in the form interpretable by R; e.g., lnorm
+#' instead of log-normal.
+#'
+#' @format A tibble with two variables:
+#' \describe{
+#'   \item{rdist}{Distribution given in a form interpretable by R.}
+#'   \item{rpar}{Parameters given as a list.}
+#' }
+"distributions"
